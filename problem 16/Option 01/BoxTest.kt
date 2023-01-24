e40fcbf67366ecfaf4bffcb5e542edb2660379a2
Aman Tairo <aman6montairo@outlook.com>
2023-01-24 07:47:19 +0300

    #vB oop

--- problem 16/Option 01/BoxTest.kt
+++ problem 16/Option 01/BoxTest.kt
@@ -1,20 +1,20 @@
-
-class Box() {
-    var length:Int
-    var width:Int
-    var height:Int
-
-    constructor(hg:Int, ln:Int, wd:Int){
-        length = ln
-        width = wd
-        height = hg
-    }
-    fun calculateVolume() {
-        return length*width*height
+class Box(val length: Double, val width: Double, val height: Double) {
+    fun calculateVolume(): Double {
+        return length * width * height
     }
 }
 
-fun main(args: Array<String>) {
-    val myBox = Box(7, 8, 3)
-    println("The volume of a box is : ${myBox.calculateVolume()}") 
+
+fun main() {
+    val box1 = Box(10.0, 20.0, 30.0)
+    val volume1 = box1.calculateVolume()
+    println("Volume of box1: $volume1")
+
+    val box2 = Box(5.0, 10.0, 15.0)
+    val volume2 = box2.calculateVolume()
+    println("Volume of box2: $volume2")
+
+    val box3 = Box(3.0, 6.0, 9.0)
+    val volume3 = box3.calculateVolume()
+    println("Volume of box3: $volume3")
 }
--- problem 16/Option 01/BoxTest.vb
+++ problem 16/Option 01/BoxTest.vb
@@ -1,9 +1,32 @@
-VERSION 1.0 CLASS
-  BEGIN
-  MultiUse = -1  'True
-  END
-  Attribute VB_Name = ""
-  Attribute VB_GlobalNameSpace = False
-  Attribute VB_Creatable = False
-  Attribute VB_PredeclaredId = False
-  Attribute VB_Exposed = False
+Public Class Box
+    Private _length As Double
+    Private _width As Double
+    Private _height As Double
+
+    Public Sub New(ByVal l As Double, ByVal w As Double, ByVal h As Double)
+        _length = l
+        _width = w
+        _height = h
+    End Sub
+
+    Public Function CalculateVolume() As Double
+        Return _length * _width * _height
+    End Function
+End Class
+
+
+Module BoxText
+    Sub Main()
+        Dim box1 As New Box(10, 20, 30)
+        Dim volume1 As Double = box1.CalculateVolume()
+        Console.WriteLine("Volume of box1: " & volume1)
+
+        Dim box2 As New Box(5, 10, 15)
+        Dim volume2 As Double = box2.CalculateVolume()
+        Console.WriteLine("Volume of box2: " & volume2)
+
+        Dim box3 As New Box(3, 6, 9)
+        Dim volume3 As Double = box3.CalculateVolume()
+        Console.WriteLine("Volume of box3: " & volume3)
+    End Sub
+End Module
